081eb48ccb17e3dd3cbb31198017c08c6ee39760
joaobarata <14177189+joaobarata@users.noreply.github.com>
2026-08-10 20:49:14 +0100

    Add new build script & update to .net10

--- OpenPGP/generate_upload_package.ps1
+++ OpenPGP/generate_upload_package.ps1
@@ -1,2 +1,2 @@
-dotnet publish -c Release -r linux-x64 --self-contained false
+dotnet publish -c Release -f net10.0 -r linux-x64 --no-self-contained
 Compress-Archive -Path .\bin\Release\net8.0\linux-x64\publish\* -DestinationPath OpenPGP.zip -update
